9c68fc30dce92d85783720c243f42201cfb15da3
Georges Winterstein <winter.georges@gmail.com>
2021-12-06 14:42:11 +0100

    mise en forme du code

--- R/fn21_enregistre_tableaux.R
+++ R/fn21_enregistre_tableaux.R
@@ -12,6 +12,75 @@ fn21_enregistre_tableaux <- function(x = ls_dates$annee_etude) {
 
   # tab_calculs_metadonnees <-
   #   readr::read_csv('E:/A_Bureau/R/EPTB/3_tables/tab_calculs_metadonnees.csv')
+  #
+
+  # tableau comparatif terrains dep cor vs corse et france
+  #
+
+  tli_frcordepcor_filtre_ssfiltre[["evol_an"]][["doc_frcordep_filtre"]] %>%
+    dplyr::select(territoire, indic, value) %>%
+    dplyr::mutate(type = dplyr::case_when(
+      indic %in% c(ind_dec, "cout_projet", "surf_m2") ~ "pc",
+      indic %in% c("part_projet") ~
+        "diff"
+    )) %>%
+    dplyr::filter(complete.cases(type)) -> eff1
+
+  c("pc", "diff") -> mes_param
+
+  fn_diff_fr_cor <- function(x = "pc") {
+    eff1 %>% dplyr::filter(type %in% x) %>%
+      dplyr::filter(territoire %in% c("Corse-du-Sud", "Haute-Corse")) -> eff1_dep
+    eff1 %>% dplyr::filter(type %in% x) %>%
+      dplyr::filter(territoire %in% c("France")) %>%
+      dplyr::select(-territoire) %>% dplyr::rename(c("fr" = "value")) -> eff1_fr
+    eff1 %>% dplyr::filter(type %in% x) %>%
+      dplyr::filter(territoire %in% c("Corse")) %>%
+      dplyr::select(-territoire) %>% dplyr::rename(c("cor" = "value")) -> eff1_cor
+
+    eff1_dep %>% dplyr::left_join(eff1_fr, by = c("indic", "type")) %>%
+      dplyr::left_join(eff1_cor, by = c("indic", "type")) %>%
+      dplyr::mutate(d_dep_cor = value - cor,
+                    d_dep_fr = value - fr) -> eff1_dep
+    if (unique(eff1_dep$type) == "pc")
+      eff1_dep %>%
+      dplyr::mutate(dt_dep_cor = d_dep_cor / cor,
+                    dt_dep_fr = d_dep_fr / fr) -> eff1_dep
+    return(eff1_dep)
+  }
+
+  purrr::map_dfr(mes_param %>% dplyr::pull(type), fn_diff_fr_cor) -> tab_terrains_compare_dep_fr
+  rm(eff1_dep, eff1_fr, eff1_cor, eff1)
+
+  # tableau comparatif maisons dep cor vs corse et france
+  #
+
+  tli_frcordepcor_filtre_ssfiltre[["evol_an"]][["doc_frcordep_ssfiltre"]] %>%
+    dplyr::select(territoire, indic, value) %>%
+    dplyr::mutate(type = dplyr::case_when(indic %in% c(ind_dec, "surf_m2") ~
+                                            "pc",
+                                          TRUE ~ NA_character_)) %>%
+    dplyr::filter(complete.cases(type)) -> eff1
+
+  x = "pc"
+  eff1 %>% dplyr::filter(type %in% x) %>%
+    dplyr::filter(territoire %in% c("Corse-du-Sud", "Haute-Corse")) -> eff1_dep
+  eff1 %>% dplyr::filter(type %in% x) %>%
+    dplyr::filter(territoire %in% c("France")) %>%
+    dplyr::select(-territoire) %>% dplyr::rename(c("fr" = "value")) -> eff1_fr
+  eff1 %>% dplyr::filter(type %in% x) %>%
+    dplyr::filter(territoire %in% c("Corse")) %>%
+    dplyr::select(-territoire) %>% dplyr::rename(c("cor" = "value")) -> eff1_cor
+
+  eff1_dep %>% dplyr::left_join(eff1_fr, by = c("indic", "type")) %>%
+    dplyr::left_join(eff1_cor, by = c("indic", "type")) %>%
+    dplyr::mutate(d_dep_cor = value - cor,
+                  d_dep_fr = value - fr) %>%
+    dplyr::mutate(dt_dep_cor = d_dep_cor / cor,
+                  dt_dep_fr = d_dep_fr / fr) -> tab_maisons_compare_dep_fr
+
+  rm(eff1_dep, eff1_fr, eff1_cor, eff1)
+
 
   # tableau bilan
   tab_calculs <-
@@ -22,11 +91,13 @@ fn21_enregistre_tableaux <- function(x = ls_dates$annee_etude) {
       "terrains_themes" = tab2_terrains,
       "terrains_autres_reg" = ls_beyond$terrains,
       "terrains_autres_reg_clt" = ls_beyond$terrains_clt,
+      "terrains_depcor_compare_fr" = tab_terrains_compare_dep_fr,
       "maisons_an" = tli_frcordepcor_filtre_ssfiltre[["evol_an"]][["doc_frcordep_ssfiltre"]],
       "maisons_depuis2010" = tli_frcordepcor_filtre_ssfiltre[["evol_depuis2010"]][["doc_frcordep_ssfiltre"]],
       "maisons_themes" = tab4_maisons,
       "maisons_autres_reg" = ls_beyond$maisons,
-      "maisons_autres_reg_clt" = ls_beyond$maisons_clt
+      "maisons_autres_reg_clt" = ls_beyond$maisons_clt,
+      "maisons_depcor_compare_fr" = tab_maisons_compare_dep_fr
     )
 
   rm(ls_beyond)
@@ -37,12 +108,14 @@ fn21_enregistre_tableaux <- function(x = ls_dates$annee_etude) {
   if (exists("tab4_maisons_fr") == TRUE)
     tab_calculs[["maisons_fr_indics_an"]] <- tab4_maisons_fr
 
-  rio::export(tab_calculs,
-              file.path("4_resultats",
-                        ls_dates[["annee_etude"]],
-                        "Tableaux",
-                        "tab_calculs.xlsx"),
-              overwrite = TRUE)
+  rio::export(
+    tab_calculs,
+    file.path("4_resultats",
+              ls_dates[["annee_etude"]],
+              "Tableaux",
+              "tab_calculs.xlsx"),
+    overwrite = TRUE
+  )
 
   readr::write_rds(tab_calculs,
                    file.path("4_resultats",
