2347c397ff7fe28c22a080582599316e4dc46b15
ggwinter <49496645+ggwinter@users.noreply.github.com>
2022-01-11 15:45:26 +0100

    reload package modifie import fn40

--- R/fn99_utilitaire_importFrom.R
+++ R/fn99_utilitaire_importFrom.R
@@ -14,7 +14,7 @@ fn99_utilitaire_importFrom <- function(x = 'fn06_met_en_forme_liste.R') {
   # list.files(file.path("R"), pattern =".R")
   readLines(file.path("R", x))-> txt
 
-  stringr::str_extract_all(txt, "[:word:]*::[:word:]{3,15}(?=\\()") %>% as.list() -> extract
+  stringr::str_extract_all(txt, "[:word:]*::[:word:].{3,15}(?=\\()") %>% as.list() -> extract
   extract[which(purrr::map(extract, length) != 0) %>% unlist()] %>% unlist() %>%
     unique() %>% sort() %>% stringr::str_replace(., "::", " ")-> extract
   return(extract)
