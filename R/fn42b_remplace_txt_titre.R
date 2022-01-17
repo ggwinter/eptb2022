c36b962a82996c8908c48ea242ddb890385b45aa
ggwinter <49496645+ggwinter@users.noreply.github.com>
2022-01-17 14:24:03 +0100

    modifie fn42b

--- R/fn42b_remplace_txt_titre.R
+++ R/fn42b_remplace_txt_titre.R
@@ -3,23 +3,25 @@
 #' @param x la date de l'annee d'etude
 #'
 #' @return
+#' @importFrom dplyr filter
+#' @importFrom dplyr pull
+#' @importFrom stringr  str_detect
+#' @importFrom utf8 as_utf8
 #' @importFrom xml2 xml_set_attr
 #' @importFrom xml2 xml_find_all
 #' @importFrom xml2 xml_children
-#' @importFrom stringr  str_detect
-#' @importFrom dplyr pull
-#' @importFrom utf8 as_utf8
 #' @export
 fn42b_remplace_txt_titre <- function(x = ls_dates$annee_etude) {
   ls_modele$t_objets_numero %>%
     dplyr::filter(stringr::str_detect(nom_objet, "_titre$|_Titre$")) %>%
-    pull(num_objet) -> num_node_titre
+    dplyr::pull(num_objet) -> num_node_titre
 
   titre_new <- utf8::as_utf8(c("Prix des terrains à b\u00e2tir", paste0("en ", x)))#"Prix des terrains \u00e0 b\u00e2tir"
 
 
   xml_set_attr(
-    ls_modele$pg %>% xml2::xml_find_all(".//PAGEOBJECT") %>%
+    ls_modele$pg %>%
+      xml2::xml_find_all(".//PAGEOBJECT") %>%
       .[[num_node_titre]]  %>%
       xml2::xml_children() %>%
       xml2::xml_find_all("ITEXT"),
