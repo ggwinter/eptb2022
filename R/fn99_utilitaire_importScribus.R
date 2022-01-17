15b96af0ce2bbe3ded70be3dc23cf16e753e821a
ggwinter <49496645+ggwinter@users.noreply.github.com>
2022-01-17 12:05:11 +0100

    enregistrement modele plaquette dans pkg preciser encoding

--- R/fn99_utilitaire_importScribus.R
+++ R/fn99_utilitaire_importScribus.R
@@ -8,8 +8,9 @@
 #' @importFrom usethis use_data
 fn99_utilitaire_importScribus <- function(x) {
   # "E:/A_Bureau/R/EPTB/3_tables/modeles_ne_pas_modifier/p4p_eptb_V.sla"
-  readLines(file.path(x))-> txt_plaquette
+  readLines(file.path(x), encoding = "UTF-8")-> txt_plaquette
   paste(txt_plaquette, collapse = " ")-> txt_plaquette
   usethis::use_data(txt_plaquette, overwrite = TRUE)
 
 }
+
